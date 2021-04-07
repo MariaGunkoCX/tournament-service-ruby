a4e9f22cfda7621b49995b8329275b1eee01e723
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-07 14:38:00 +0300

    initial tournament controller creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'tournaments/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tournaments do
+    member do
+    end
+  end
+
 end
