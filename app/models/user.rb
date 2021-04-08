6713088e7750bfa5ad37c87df0405f8b1ddbdf18
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-08 18:18:18 +0300

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates_presence_of :first_name, :last_name, :email
+    validates_length_of :first_name, :maximum => 25
+    validates_length_of :last_name, :maximum => 25
+    validates_uniqueness_of :email
 end
