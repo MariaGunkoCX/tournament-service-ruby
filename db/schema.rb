9a51dc817a5c133d493606c371a8feda3e746c9f
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-08 10:59:24 +0300

    added migration for results

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210408071020) do
+ActiveRecord::Schema.define(version: 20210408072316) do
+
+  create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.json    "answers"
+    t.integer "user_id"
+    t.integer "tournament_id"
+    t.index ["tournament_id"], name: "index_results_on_tournament_id", using: :btree
+    t.index ["user_id"], name: "index_results_on_user_id", using: :btree
+  end
 
   create_table "tournaments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "company_name"
+    t.string   "company_name", null: false
     t.datetime "start_date"
     t.datetime "end_date"
   end
@@ -24,4 +32,6 @@ ActiveRecord::Schema.define(version: 20210408071020) do
     t.string "email",                 default: "", null: false
   end
 
+  add_foreign_key "results", "tournaments"
+  add_foreign_key "results", "users"
 end
