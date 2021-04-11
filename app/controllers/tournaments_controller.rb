20f09eb217ea2507d6c4d0ddf2da388054bd8179
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-11 13:15:09 +0300

    try-catch added

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -8,20 +8,28 @@ class TournamentsController < ApplicationController
   end
 
   def create
-    new_tournament = Tournament.create!(company_name: params[:company_name], start_date: params[:start_date], end_date: params[:end_date])
-    results = params[:results]
-    results.each do |result|
-      new_user = User.create!(first_name: result[:firstName], last_name: result[:lastName], email: result[:email])
-      temp = Result.create!(user_id: new_user.id, tournament_id: new_tournament.id , answers: result[:answers])
+    begin
+      new_tournament = Tournament.create!(company_name: params[:company_name], start_date: params[:start_date], end_date: params[:end_date])
+      results = params[:results]
+      results.each do |result|
+        new_user = User.create!(first_name: result[:firstName], last_name: result[:lastName], email: result[:email])
+        temp = Result.create!(user_id: new_user.id, tournament_id: new_tournament.id , answers: result[:answers])
+      end
+    rescue 
+      render json: "message: error occurred", status: :bad_request
     end
   end
 
   def show
-    current_tournament = Tournament.find(params[:id])
-    final_data = JSON.parse(current_tournament.to_json)
-    answers = Result.joins(:user).select("users.*,results.answers").where(results: {tournament_id: params[:id]})
-    final_data["results"] = JSON.parse(answers.to_json)
-    render json: final_data, status: :ok
+    begin
+      current_tournament = Tournament.find(params[:id])
+      final_data = JSON.parse(current_tournament.to_json)
+      answers = Result.joins(:user).select("users.*,results.answers").where(results: {tournament_id: params[:id]})
+      final_data["results"] = JSON.parse(answers.to_json)
+      render json: final_data, status: :ok 
+    rescue
+      render json: "message: error occurred", status: :bad_request
+    end
   end
 
   def statistics
