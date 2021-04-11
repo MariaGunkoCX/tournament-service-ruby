085585cfc40633ed2b684741552eea3fd7b76ecf
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-11 10:00:11 +0300

    saveTournamentResults api done

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -6,7 +6,12 @@ class TournamentsController < ApplicationController
   end
 
   def create
-    Tournament.create!(company_name: params[:company_name], start_date: params[:start_date], end_date: params[:end_date])
+    new_tournament = Tournament.create!(company_name: params[:company_name], start_date: params[:start_date], end_date: params[:end_date])
+    results = params[:results]
+    results.each do |result|
+      new_user = User.create!(first_name: result[:firstName], last_name: result[:lastName], email: result[:email])
+      temp = Result.create!(user_id: new_user.id, tournament_id: new_tournament.id , answers: result[:answers])
+    end
   end
 
   def show
