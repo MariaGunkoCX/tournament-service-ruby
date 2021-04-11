b8a3ecf8e8a25b22d54792fde141c1fcb35eee20
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-11 17:04:08 +0300

    success calculation method moved to helper

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,6 +2,7 @@ require 'json'
 
 class TournamentsController < ApplicationController
 
+  include TournamentsHelper
   skip_before_action :verify_authenticity_token
 
   def index
@@ -42,37 +43,6 @@ class TournamentsController < ApplicationController
     results = Result.select("results.answers").where(tournament_id: params[:id])
     statistics = successPerQuestion(results)
     render json: statistics, status: :ok 
-  end
-
-  def successPerQuestion(results)
-    statistics = Array.new;
-    questions = Array.new;
-
-    # initialize empty array
-    for index in 0..9 do
-      questions[index] = 0;
-    end
-
-    results.each do |result|
-      # count all true questions
-      for index in 1..10 do
-        if (result.answers[index.to_s])
-          questions[index-1] = questions[index-1] + 1
-        end
-      end
-    end
-
-    # create statistics object
-    for index in 1..10 do
-      singleQuestion = {
-        question: index.to_s,
-        percentage: (questions[index-1]/results.length()*100).to_s + "%"
-      }
-      statistics.append(singleQuestion)
-    end
-
-    return statistics
-  
   end
 
 end
--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,34 @@
 module TournamentsHelper
+
+    def successPerQuestion(results)
+        statistics = Array.new;
+        questions = Array.new;
+    
+        # initialize empty array
+        for index in 0..9 do
+          questions[index] = 0;
+        end
+    
+        results.each do |result|
+          # count all true questions
+          for index in 1..10 do
+            if (result.answers[index.to_s])
+              questions[index-1] = questions[index-1] + 1
+            end
+          end
+        end
+    
+        # create statistics object
+        for index in 1..10 do
+          singleQuestion = {
+            question: index.to_s,
+            percentage: (questions[index-1]/results.length()*100).to_s + "%"
+          }
+          statistics.append(singleQuestion)
+        end
+    
+        return statistics
+      
+      end
+      
 end
