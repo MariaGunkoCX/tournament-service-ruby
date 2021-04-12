2572454bd2b679e945e9f86ae57b7dfbbca3e06c
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-12 13:34:50 +0300

    calculator refactor

--- app/services/statistics_calculator.rb
+++ app/services/statistics_calculator.rb
@@ -6,27 +6,30 @@ class StatisticsCalculator
             statistics = Array.new;
             questions = Array.new(10,0);
             
-            results.each do |result|
-            # count all true questions
-            for index in 1..10 do
-                if (result.answers[index.to_s])
-                questions[index-1] = questions[index-1] + 1
-                end
-            end
-            end
-        
-            # create statistics object
-            for index in 1..10 do
-            single_question = {
-                question: index.to_s,
-                percentage: (questions[index-1]/results.length()*100).to_s + "%"
-            }
-            statistics << single_question
-            end
-        
+            results.each {|result| count_correct_answers_in_result(result.answers, questions)}
+            claculate_correct_answers_percentage(questions, results.length(), statistics)
             statistics
-        
+
         end
+
+        def count_correct_answers_in_result(result_answers, questions)
+            for index in 1..10 do
+                if (result_answers[index.to_s])
+                    questions[index-1] += 1
+                end
+             end
+        end
+
+        def claculate_correct_answers_percentage(questions, results_length, statistics)
+            for index in 1..10 do
+                single_question = {
+                    question: index.to_s,
+                    percentage: (questions[index-1]/results_length*100).to_s + "%"
+                }
+                statistics << single_question
+            end
+        end
+
     end
 
 end
