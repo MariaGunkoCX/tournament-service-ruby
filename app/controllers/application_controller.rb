f440a3e4a8154c93d3ff54dfc86748d9b3fec4b5
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-13 11:00:01 +0300

    rescue_from invalid record added to application helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     render json: { error: "Internal Server Error" }, status: :internal_server_error
   end
 
+  rescue_from ActiveRecord::RecordInvalid do |invalid|
+    render json: {error: "Failed to create a new tournament, with error: #{invalid.record.errors.full_messages} - #{invalid.record.errors.details}"}, status: :internal_server_error
+  end
+
 end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -9,19 +9,15 @@ class TournamentsController < ApplicationController
   end
 
   def create
-    begin
-      Tournament.transaction do
-        new_tournament = Tournament.create!(company_name: params[:company_name], start_date: params[:start_date], end_date: params[:end_date])
-        results = params[:results]
-        results.each do |result|
-          new_user = User.create!(first_name: result[:firstName], last_name: result[:lastName], email: result[:email])
-          temp = Result.create!(user_id: new_user.id, tournament_id: new_tournament.id , answers: result[:answers])
-        end
-      end 
-      render json: { success: "New Tournament Created" }, status: :ok 
-    rescue ActiveRecord::RecordInvalid => invalid
-      render json: {error: "Failed to create a new tournament, with error: #{invalid.record.errors.full_messages} - #{invalid.record.errors.details}"}, status: :internal_server_error
-    end
+    Tournament.transaction do
+      new_tournament = Tournament.create!(company_name: params[:company_name], start_date: params[:start_date], end_date: params[:end_date])
+      results = params[:results]
+      results.each do |result|
+        new_user = User.create!(first_name: result[:firstName], last_name: result[:lastName], email: result[:email])
+        temp = Result.create!(user_id: new_user.id, tournament_id: new_tournament.id , answers: result[:answers])
+      end
+    end 
+    render json: { success: "New Tournament Created" }, status: :ok 
   end
 
   def show
