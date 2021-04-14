33891baf9d01653c04b0e52300bf6786fd89881b
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-14 09:30:45 +0300

    added mapping results for success

--- app/services/statistics_calculator.rb
+++ app/services/statistics_calculator.rb
@@ -14,12 +14,9 @@ class StatisticsCalculator
     class << self 
 
         def success_per_question(results)
-            statistics = Array.new;
-            questions = Array.new(Constants::NUMBER_OF_QUESTIONS, 0);
-            
+            questions = Array.new(Constants::NUMBER_OF_QUESTIONS, 0)  
             results.each {|result| count_correct_answers_in_result(result.answers, questions)}
-            claculate_correct_answers_percentage(questions, results.length(), statistics)
-            statistics
+            questions.map.with_index {|question, index| claculate_correct_answers_percentage(question, index, results.length())}
         end
 
         def user_score(results)
@@ -36,14 +33,12 @@ class StatisticsCalculator
              end
         end
 
-        def claculate_correct_answers_percentage(questions, results_length, statistics)
-            for index in 1..Constants::NUMBER_OF_QUESTIONS do
-                single_question = {
-                    question: index.to_s,
-                    percentage: ((questions[index-1].to_f/results_length*Constants::FULL_PERCENTAGE).to_i).to_s + "%"
-                }
-                statistics << single_question
-            end
+        def claculate_correct_answers_percentage(question, index, results_length)
+            single_question = {
+                question: (index.to_i+1).to_s,
+                percentage: ((question.to_f/results_length*Constants::FULL_PERCENTAGE).to_i).to_s + "%"
+            }
+            single_question
         end
 
         def count_correct_answers_for_user(result_answers, result_user)
