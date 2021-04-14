9ce3c60f76f69cde62215c2d7f4a34e1c7b230cf
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-14 14:40:51 +0300

    initial spec files added

---
+++ app/spec/statistics_calculator_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../services/statistics_calculator'
+
+describe 'statistics_calculator' do
+  it 'calculates success per question statistics' do
+  end
+
+  it 'calculates score per user statistics' do
+  end
+end
---
+++ app/spec/tournaments_controller_spec.rb
@@ -0,0 +1,16 @@
+describe 'tournaments_controller' do
+  it 'saves tournament results' do
+  end
+
+  it 'gett tournament results' do
+  end
+
+  it 'fetches success per question' do
+  end
+
+  it 'fetches users scores' do
+  end
+
+  it 'fetches tournament statistics' do
+  end
+end
