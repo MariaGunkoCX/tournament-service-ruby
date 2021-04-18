4ff899f551b2931180254ee1dcaab323891b80b0
Maria Gunko <maria.gunko@checkmarx.com>
2021-04-18 19:14:23 +0300

    controller initial spec

--- spec/controllers/tournaments_controller_spec.rb
+++ spec/controllers/tournaments_controller_spec.rb
@@ -1,16 +1,45 @@
-describe 'tournaments_controller' do
-  it 'saves tournament results' do
+require 'rails_helper'
+
+describe TournamentsController do
+  before(:all) do
+    @tournament = build(:tournament)
   end
 
-  it 'get tournament results' do
+  context 'input is valid' do
+    it 'saves tournament results' do
+    end
+
+    it 'get tournament results' do
+    end
+
+    it 'fetches success per question' do
+    end
+
+    it 'fetches users scores' do
+    end
+
+    it 'fetches tournament statistics' do
+    end
   end
 
-  it 'fetches success per question' do
-  end
+  context 'input is not valid' do
+    it 'saves tournament results' do
+      post :create, params: { tournament: @tournament }
+      expect(response).to have_http_status(500)
+    end
 
-  it 'fetches users scores' do
-  end
+    it 'get tournament results' do
+      get :show, params: { id: '100' }
+      expect(response).to have_http_status(404)
+    end
 
-  it 'fetches tournament statistics' do
+    it 'fetches success per question' do
+    end
+
+    it 'fetches users scores' do
+    end
+
+    it 'fetches tournament statistics' do
+    end
   end
 end
---
+++ spec/factories/tournament.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :tournament do
+    company_name { 'Checkmarx' }
+    start_date { '2020-18-04T18:28:00+00:00' }
+    end_date { '2020-18-04T18:28:00+00:00' }
+  end
+end
